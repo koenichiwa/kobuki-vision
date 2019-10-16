085047699b081b4913ea0ecb9904862541567492
Mellow <melle.toxopeus@gmail.com>
2019-10-16 15:34:44 +0200

    Distance to object node

--- src/dto_node/dto_node.cpp
+++ src/dto_node/dto_node.cpp
@@ -15,18 +15,33 @@ using namespace pcl;
 using namespace ros;
 using namespace sensor_msgs;
 
-void depthPointsCallback(const PointCloud2 &msg) {
-    PointCloud<PointXYZ> pc;
-    fromROSMsg(msg, pc);
-    PointXYZ p = pc.at(msg.width / 2, msg.height / 2);
-    cout << "X: " << p.x << ", Y: " << p.y << ", Z: " << p.z << endl;
-}
+class DistanceToObject{
+
+private:
+    int Xpos = 0;
+    int Ypos = 0;
+
+public:
+    void depthPointsCallback(const PointCloud2 &msg) {
+        PointCloud<PointXYZ> pc;fromROSMsg(msg, pc);
+        PointXYZ p = pc.at(this->Xpos , this->Ypos);
+        cout << "X: " << p.x << ", Y: " << p.y << ", Z: " << p.z << endl;
+    }
+
+    void XYcallback(int i){
+
+    }
+
+};
 
 int main(int argc, char **argv){
     init(argc, argv, "dto_node");
     NodeHandle n("~");
     Rate loop_rate(1);
-    Subscriber depthPointsSub = n.subscribe("/camera/depth/points", 10, depthPointsCallback);
+
+    DistanceToObject DTO;
+    Subscriber depthPointsSub = n.subscribe("/camera/depth/points", 10, &DistanceToObject::depthPointsCallback, &DTO);
+    Subscriber XYsub = n.subscribe("/get/x/y", 10, &DistanceToObject::depthPointsCallback, &DTO);
 
     while (ok()) {
         spinOnce();
