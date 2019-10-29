b298f56c1696edfa4cb1ac6a1d3447d41cde6f7a
Bo Sterenborg <bosterenborg@gmail.com>
2019-10-29 12:54:26 -0700

    Added some code which detects persons and determines distance towards them

--- src/dto_node/dto_node.cpp
+++ src/dto_node/dto_node.cpp
@@ -7,9 +7,13 @@
 #include <pcl/point_types.h>
 #include <pcl/PCLPointCloud2.h>
 #include <pcl/conversions.h>
+#include "opencv2/opencv.hpp"
 #include "message_filters/time_synchronizer.h"
 #include "vision/ObjectPosition.h"
 #include "message_filters/subscriber.h"
+#include "cv_bridge/cv_bridge.h"
+#include "opencv2/objdetect.hpp"
+
 #define MAX_QUEUE_SIZE 10
 
 using namespace std;
@@ -18,13 +22,37 @@ using namespace ros;
 using namespace sensor_msgs;
 using namespace message_filters;
 using namespace vision;
+using namespace cv_bridge;
+using namespace cv;
 
-
-void callBack(const PointCloud2 &pointCloud, const ObjectPosition &pos) {
+void callBack(const PointCloud2ConstPtr &pointCloud, const ImageConstPtr &im) {
     PointCloud<PointXYZ> pc;
-    fromROSMsg(pointCloud, pc);
-    PointXYZ p = pc.at(pos.x, pos.y);
-    cout << "X: " << p.x << ", Y: " << p.y << ", Z: " << p.z << endl;
+    fromROSMsg(*pointCloud, pc);
+
+    //todo 29/10/2019 this is duplicate and needs to be handled by img_conv_node
+    CvImagePtr cv_ptr;
+    try {
+        cv_ptr = toCvCopy(*im, image_encodings::BGR8);
+        HOGDescriptor hogDet;
+        hogDet.setSVMDetector(HOGDescriptor::getDefaultPeopleDetector());
+        vector<Rect> found;
+        vector<double> weights;
+
+        hogDet.detectMultiScale(cv_ptr->image, found, weights);
+        if (!found.empty()) {
+            for (size_t i = 0; i < found.size(); i++) {
+                Rect r = found.at(i);
+                PointXYZ p = pc.at(r.x, r.y);
+                cout << "Persoon " << i << "; X: " << p.x << ", Y: " << p.y << ", Z: " << p.z << endl;
+            }
+        } else {
+            cout << "Er zijn geen personen gevonden..." << endl;
+        }
+    }
+    catch (cv_bridge::Exception &e) {
+        ROS_ERROR("cv_bridge exception: %s", e.what());
+        return;
+    }
 }
 
 int main(int argc, char **argv) {
@@ -33,9 +61,9 @@ int main(int argc, char **argv) {
     Rate loop_rate(1);
 
     message_filters::Subscriber<PointCloud2> depthPointsSub(n, "/camera/depth/points", MAX_QUEUE_SIZE);
-    message_filters::Subscriber<ObjectPosition> objPosSub(n, "/some/object/position", MAX_QUEUE_SIZE);
+    message_filters::Subscriber<Image> imageSub(n, "/camera/rgb/image_raw", MAX_QUEUE_SIZE);
 
-    TimeSynchronizer<PointCloud2, ObjectPosition> sync(depthPointsSub, objPosSub, MAX_QUEUE_SIZE);
+    TimeSynchronizer<PointCloud2, Image> sync(depthPointsSub, imageSub, MAX_QUEUE_SIZE);
     sync.registerCallback(boost::bind(&callBack, _1, _2));
 //
     while (ok()) {
