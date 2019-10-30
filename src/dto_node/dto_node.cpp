b1b6dee6e7f48182890136178a23e92714c5219a
Bo Sterenborg <bosterenborg@gmail.com>
2019-10-30 02:36:04 -0700

    Documented properly

--- src/dto_node/dto_node.cpp
+++ src/dto_node/dto_node.cpp
@@ -8,7 +8,6 @@
 #include <pcl/PCLPointCloud2.h>
 #include <pcl/conversions.h>
 #include "opencv2/opencv.hpp"
-#include "message_filters/time_synchronizer.h"
 #include "message_filters/subscriber.h"
 #include "cv_bridge/cv_bridge.h"
 #include "opencv2/objdetect.hpp"
@@ -24,6 +23,11 @@ using namespace message_filters;
 using namespace cv_bridge;
 using namespace cv;
 
+/**
+ * Takes a PointCloud and image, then calculates the distance towards detected objects.
+ * @param pointCloud = astra's PointCloud2
+ * @param im = astra's image
+ */
 void callBack(const PointCloud2ConstPtr &pointCloud, const ImageConstPtr &im) {
     PointCloud<PointXYZ> pc;
     fromROSMsg(*pointCloud, pc);
@@ -32,17 +36,20 @@ void callBack(const PointCloud2ConstPtr &pointCloud, const ImageConstPtr &im) {
     CvImagePtr cv_ptr;
     try {
         cv_ptr = toCvCopy(*im, image_encodings::BGR8);
+
+        //timely detect people, future should support more
         HOGDescriptor hogDet;
         hogDet.setSVMDetector(HOGDescriptor::getDefaultPeopleDetector());
-        vector<Rect> found;
+        vector<Rect> peopleRectangles;
         vector<double> weights;
 
-        hogDet.detectMultiScale(cv_ptr->image, found, weights);
-        if (!found.empty()) {
-            for (size_t i = 0; i < found.size(); i++) {
-                Rect r = found.at(i);
+        //Save detected people into rectangles
+        hogDet.detectMultiScale(cv_ptr->image, peopleRectangles, weights);
+        if (!peopleRectangles.empty()) {
+            for (size_t i = 0; i < peopleRectangles.size(); i++) {
+                Rect r = peopleRectangles.at(i);
                 PointXYZ p = pc.at(r.x, r.y);
-                cout << "Persoon " << i+1 << "; X: " << p.x << ", Y: " << p.y << ", Z: " << p.z << endl;
+                cout << "Persoon " << i + 1 << "; X: " << p.x << ", Y: " << p.y << ", Z: " << p.z << endl;
             }
         } else {
             cout << "Er zijn geen personen gevonden..." << endl;
@@ -62,10 +69,12 @@ int main(int argc, char **argv) {
     message_filters::Subscriber<PointCloud2> depthPointsSub(n, "/camera/depth/points", MAX_QUEUE_SIZE);
     message_filters::Subscriber<Image> imageSub(n, "/camera/rgb/image_raw", MAX_QUEUE_SIZE);
 
+    //The 'approximate' syncpolicy is required to get an image and pointcloud
+    //The timestamps are note fully synchronized...
     typedef sync_policies::ApproximateTime<PointCloud2, Image> syncPolicy;
-    Synchronizer<syncPolicy> sync(syncPolicy(10), depthPointsSub, imageSub);
+    Synchronizer<syncPolicy> sync(syncPolicy(MAX_QUEUE_SIZE), depthPointsSub, imageSub);
     sync.registerCallback(boost::bind(&callBack, _1, _2));
-//
+
     while (ok()) {
         spinOnce();
         fflush(stdout);
