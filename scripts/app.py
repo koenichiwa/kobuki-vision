69c523b71b0ef6340d5212040ec26cfc5f1cdcb2
Mellow <melle.toxopeus@gmail.com>
2019-12-04 16:26:21 +0100

    fixed error where json wasnt properly constructed

--- scripts/app.py
+++ scripts/app.py
@@ -1,29 +1,30 @@
-from flask import Flask
+#!/usr/bin/env python
+from flask import Flask, render_template
+from flask_cors import CORS
 import threading
 import rospy
 from vision.msg import ObjectPosition
 import json
 
-detected_objects = []
-
 
 class DetectedObject:
+    """
+    This class is used to efficiently store the objects until they are requested by the webserver
+    """
 
     def __init__(self, name, x, y, z, distance):
         self.name = name
         self.x, self.y, self.z = x, y, z
         self.distance = distance
 
-    def __dict__(self):
-        return {
-            "type": self.name,
-            "x": self.x,
-            "y": self.y,
-            "z": self.z,
-            "distance": self.distance
-        }
-
 def callback(data):
+    """
+    This callback is called when a new message is recieved, creates a new DetectedObjects instance and adds it to the
+    list if the list is full the item that was first added is removed
+
+    :param data: Data recieved from ros message
+    :return:
+    """
     obj = DetectedObject(data.type, data.x, data.y, data.z, data.distance)
 
     if len(detected_objects) < 10:
@@ -32,25 +33,33 @@ def callback(data):
         detected_objects.pop(0)
         detected_objects.append(obj)
 
+#init threaded ROS node so that it doesnt conflict with flask
+threading.Thread(target=lambda: rospy.init_node("Object_listener", disable_signals=True, anonymous=True)).start()
+sub = rospy.Subscriber("/vision/object_position", ObjectPosition, callback=callback)
 
-
-threading.Thread(target=lambda: rospy.init_node("Object listener", disable_signals=True, anonymous=True)).start()
-sub = rospy.Subscriber("Object listener", ObjectPosition,callback=callback)
+#init  flask app and empty list for objects
 app = Flask(__name__)
-
-
+CORS(app)
+detected_objects = []
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    """
+    Simply returns the Index.html inside templates to the client
+    :return: Html file
+    """
+    return render_template('Index.html')
 
 
 @app.route('/detections')
 def get_detections():
-    data = json.dumps(detected_objects)
+    """
+    creates a json from the detected_objects list and returns is as a JSON to the client
+    :return: JSON containing the 10 most recent detected objects
+    """
 
     response = app.response_class(
-        response=json.dumps(data),
+        response=json.dumps([ob.__dict__ for ob in detected_objects]),
         status=200,
         mimetype='application/json'
     )
